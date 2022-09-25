d8d85ec73a63e0dabdc43e36f91def339561ce95
Filipe Machado <x.filipe.machado.x@gmail.com>
2022-09-24 23:22:36 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);
 			
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -29,6 +31,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
